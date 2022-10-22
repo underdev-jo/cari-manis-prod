1f6377fd71ed3df9ca27a06ca907e0f18243a710
Jojo <aditya.abdul@weekendinc.com>
2022-10-22 23:10:09 +0700

    adjust pagination

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import Header from "../components/Header/Header";
 import ModalPriceInfo from "../components/Modal/PriceInfo";
 import "../styles/globals.scss";
+import "swiper/css";
+import "swiper/css/pagination";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/home/SweetInfo.js
+++ pages/home/SweetInfo.js
@@ -1,36 +1,43 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import SwiperCore, { Autoplay, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Section from "../../components/Sections/Section";
+import style from "./SweetInfo.module.scss";
+
+SwiperCore.use([Autoplay, Pagination]);
 
 const messageList = [
   "Diabetes melitus terjadi saat kadar gula di dalam darah terlalu tinggi. Ini terjadi saat jumlah hormon insulin dalam tubuh tidak cukup untuk mengubah glukosa menjadi energi.",
-  "Setiap orang maksimal mendapatkan asupan gula sebanyak 4sdm atau 50gram per hari  - Permenkes No. 30, 2013",
+  <>
+    Maksimal asupan gula per orang sebanyak 4sdm atau 50gram per hari.
+    <br />- Permenkes No. 30, 2013.
+  </>,
+  "Kadar gula dalam minuman perlu dihitung dari takaran dan jumlah sajian",
 ];
 
 export default function SweetInfo() {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    let timing;
-    let el = document.getElementById("elKProgress");
-
-    el.classList.add("animating");
-    timing = setTimeout(() => {
-      setIndex((index + 1) % messageList.length);
-      el.classList.remove("animating");
-    }, 3000);
-
-    return () => clearTimeout(timing);
-  }, [index]);
-
+  const bulletClass = style.sweetInfoBullet;
   return (
     <Section>
-      <div className="rounded-xl overflow-hidden text-white bg-secondary relative pt-4">
-        <div className="kprogress" id="elKProgress" />
+      <div
+        className="rounded-xl overflow-hidden text-white bg-secondary relative max-h-full"
+        style={{ transition: "0.35s ease-in-out" }}
+      >
         <div className="p-4 flex items-center justify-between">
           <div className="w-[90%] max-w-[267px]">
             <div className="cm-heading h4 mb-2">Tahukah kamu?</div>
-            <div className="text-small">{messageList[index]}</div>
+            <Swiper
+              slidesPerView={1}
+              autoplay={{ delay: 3000 }}
+              pagination={{ el: `.${bulletClass}`, clickable: true }}
+            >
+              {messageList.map((message, index) => (
+                <SwiperSlide key={index}>
+                  <div className="text-small">{message}</div>
+                </SwiperSlide>
+              ))}
+              <div className={bulletClass} />
+            </Swiper>
           </div>
           <div className="flex items-center justify-center">
             <Image
