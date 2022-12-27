bc9a5da381c91e20aa1641f2252d45efd769d9a0
Jojo <aditya.abdul@weekendinc.com>
2022-12-27 10:04:09 +0700

    add scroll dir

--- layouts/ProductSearch/index.js
+++ layouts/ProductSearch/index.js
@@ -1,22 +1,39 @@
 import InputSearchDrink from "components/Input/InputSearchDrink";
-import useScrollDirection from "customHooks/useScrollDirection";
+import { useEffect, useState } from "react";
 import FilterPackaging from "./filter-packaging";
 import FilterSugar from "./filter-sugar";
 import style from "./product-search.module.scss";
-
-let lastScroll = 0;
 
 export async function getServerSideProps(context) {
   const { query } = context;
   return { props: { query } };
 }
 
+let lastScroll = 0;
+
 export default function ProductSearch({ keyword, query }) {
-  const direction = useScrollDirection();
-  const classUp = direction === "down" ? style.hide : style.shown;
+  const [scrollDir, setScrolldir] = useState("up");
+
+  useEffect(() => {
+    const fn = () => {
+      const posY = parseInt(window.pageYOffset, 10);
+      if (posY >= lastScroll) setScrolldir("down");
+      else setScrolldir("up");
+      console.log({ posY, lastScroll });
+      lastScroll = posY <= 0 ? 0 : posY;
+    };
+
+    window.addEventListener("scroll", fn);
+
+    return () => {
+      window.removeEventListener("scroll", fn);
+    };
+  }, []);
+
+  const classUp = style.shown;
 
   return (
-    <div className={style["product-search"]}>
+    <div className={`${style["product-search"]} ${style[scrollDir]}`}>
       <div className={style.inner}>
         <div className="z-50">
           <InputSearchDrink size="small" value={keyword} allowEmpty />
