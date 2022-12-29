8ae3985eb25bb165af0245f02c8779812263374c
Jojo <aditya.abdul@weekendinc.com>
2022-12-29 11:48:18 +0700

    j

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
