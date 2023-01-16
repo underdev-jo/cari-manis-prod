46df6ded6049de4e301502cffa2651c04666275b
Jojo <aditya.abdul@weekendinc.com>
2023-01-16 13:24:03 +0700

    move serversideprops

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
+import { supaKey, supaUrl } from "helpers/util";
 import DrinkList from "layouts/Product/DrinkList";
 import { Cover, DrinkCategory, SweetInfo } from "pageElement/home";
 import PageHead from "pages/PageHead";
+
+export async function getServerSideProps() {
+  const supabase = createClient(supaUrl(), supaKey());
+
+  const res = await supabase
+    .from("minuman")
+    .select("*")
+    .range(0, 9)
+    .order("created_at", { ascending: false });
+
+  if (res.error)
+    return {
+      redirect: {
+        destination: "/502",
+      },
+    };
+
+  return {
+    props: { drinkList: res.data || null },
+  };
+}
 
 export default function Home({ drinkList }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
-import { supaKey, supaUrl } from "helpers/util";
 import Home from "./home";
-
-export async function getServerSideProps() {
-  const supabase = createClient(supaUrl(), supaKey());
-
-  const res = await supabase
-    .from("minuman")
-    .select("*")
-    .range(0, 9)
-    .order("created_at", { ascending: false });
-
-  if (res.error)
-    return {
-      redirect: {
-        destination: "/502",
-      },
-    };
-
-  return {
-    props: { drinkList: res.data || null },
-  };
-}
 
 export default function index({ drinkList }) {
   return <Home drinkList={drinkList} />;
