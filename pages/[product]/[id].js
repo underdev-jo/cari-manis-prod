b09aca8d4682eb914686fcfb3d6bc8c31b19e69d
Jojo <aditya.abdul@weekendinc.com>
2023-01-16 14:01:17 +0700

    using getstaticprops

--- pages/[product]/[id].js
+++ pages/[product]/[id].js
@@ -1,27 +1,31 @@
 import { createClient } from "@supabase/supabase-js";
-import { eq, getCount } from "helpers/api";
+import { eq } from "helpers/api";
 import { supaKey, supaUrl } from "helpers/util";
 import { ProductView } from "pageElement/product";
 import AddToCalculator from "pageElement/product/AddToCalculator";
 import PageHead from "pages/PageHead";
 
-export async function getServerSideProps({ params, req }) {
+export async function getStaticPaths() {
   const supabase = createClient(supaUrl(), supaKey());
+  const res = await supabase.from("minuman").select("name,id");
+  let paths = [];
 
+  paths = res.data.map((x) => ({
+    params: { product: x.name.toString(), id: x.id.toString() },
+  }));
+
+  return {
+    paths, //indicates that no page needs be created at build time
+    fallback: "blocking", //indicates the type of fallback
+  };
+}
+
+export async function getStaticProps({ params }) {
   const { data, error } = await eq("minuman", {
     column: "id",
     value: params.id,
   });
 
-  // const commentParam = { column: "product_id", value: params.id };
-  // const count = await getCount("product_comment", commentParam);
-  // const { data: dataComm, error: errComm } = await supabase
-  //   .from("product_comment")
-  //   .select("*")
-  //   .eq("product_id", params.id)
-  //   .order("created_at", { ascending: false });
-
-  // const comment = { data: dataComm, error: errComm, count };
   const props = { product: data[0] };
 
   if (error)
@@ -34,7 +38,7 @@ export async function getServerSideProps({ params, req }) {
   return { props };
 }
 
-export default function ProductDetail({ product, comment }) {
+export default function ProductDetail({ product }) {
   if (!product) return "Loading...";
 
   return (
