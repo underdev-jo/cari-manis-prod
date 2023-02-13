fced91d2d44ea71c39b15c8913f630a66ebf5fe9
Jojo <aditya.abdul@weekendinc.com>
2023-02-14 00:02:57 +0700

    adding meta iamge

--- pages/PageHead.js
+++ pages/PageHead.js
@@ -3,11 +3,23 @@ import Head from "next/head";
 export default function PageHead({
   title = "cari-manis.id",
   description = "Cari tahu seberapa manis minumanmu",
+  image = "/legal/thumbnail-carimanis-med.jpg",
+  url = "https://cari-manis.my.id",
 }) {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="image" content={image} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={image} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="Cari Manis" />
+      <meta property="og:url" content={url} />
+      <meta name="twitter:card" content="summary_large_image" />
+
       <link rel="icon" href="/favicon-cari-manis.png" />
     </Head>
   );
