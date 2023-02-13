9573bcc0dbe05e5384f6597dc8c5e80b1ebb9147
Jojo <aditya.abdul@weekendinc.com>
2023-02-14 00:18:07 +0700

    update pagehead

--- pages/PageHead.js
+++ pages/PageHead.js
@@ -9,16 +9,20 @@ export default function PageHead({
   return (
     <Head>
       <title>{title}</title>
+      <meta name="title" content={title} />
       <meta name="description" content={description} />
-      <meta name="image" content={image} />
 
-      <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content={image} />
-      <meta property="og:description" content={description} />
-      <meta property="og:site_name" content="Cari Manis" />
       <meta property="og:url" content={url} />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
 
       <link rel="icon" href="/favicon-cari-manis.png" />
     </Head>
